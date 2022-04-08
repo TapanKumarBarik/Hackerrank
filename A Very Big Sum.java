0e9b2c8155113ea36a68d7fa633831f5f045dd3c
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-04-08 12:23:39 +0530

    Create A Very Big Sum.java

---
+++ A Very Big Sum.java
@@ -0,0 +1,11 @@
+    public static long aVeryBigSum(List<Long> ar) {
+    // Write your code here
+        long sum=0;
+
+        for(long i:ar){
+            
+            sum+=i;
+        }
+
+        return sum;
+    }
