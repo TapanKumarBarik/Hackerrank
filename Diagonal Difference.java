0d2502ba696a503e37f85f0c20362b93c98cb4cc
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-04-08 12:36:52 +0530

    Create Diagonal Difference.java

---
+++ Diagonal Difference.java
@@ -0,0 +1,20 @@
+public static int diagonalDifference(List<List<Integer>> arr) {
+    // Write your code here
+
+
+        int leftSum=0;
+
+        int rightSum=0;
+
+
+        int r=arr.size();
+
+        for(int i=0;i<r;i++){
+            
+            
+            leftSum+=arr.get(i).get(i);
+            
+            rightSum+=arr.get(r-r+i).get(r-1-i);
+        }
+        return Math.abs(leftSum-rightSum);
+    }
