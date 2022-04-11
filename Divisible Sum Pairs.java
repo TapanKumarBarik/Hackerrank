ee583882d55de52831780b7df68e2c6791e1c18d
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2022-04-11 09:19:06 +0530

    Create Divisible Sum Pairs.java

---
+++ Divisible Sum Pairs.java
@@ -0,0 +1,20 @@
+// /https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+
+
+
+   public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
+    // Write your code here
+    
+    
+    int noOfPairs=0;
+    
+    
+    for(int i=0;i<n;i++){
+        
+        for(int j=i+1;j<n;j++){
+            
+            if( (ar.get(i)+ar.get(j) )%k==0 )noOfPairs++;
+        }
+    }
+return noOfPairs;
+    }
